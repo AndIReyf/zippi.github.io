375711c6719f3dc0fae135856e3234bd714274cf
AndIReyf <andrey.prudius@hotmail.com>
2019-09-25 01:28:41 +0200

    Added header

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+
+
+});
