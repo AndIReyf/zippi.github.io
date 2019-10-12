e430ba51c630b56fa6e3c17fcdb8a151b84acc65
AndIReyf <andrey.prudius@hotmail.com>
2019-10-12 15:51:48 +0200

    Added Projects, pricing, slider popup section

--- app/js/main.js
+++ app/js/main.js
@@ -11,5 +11,21 @@ $(function(){
     autoplaySpeed: 5000,
   });
 
+  $('.slider__content').slick({
+    infinite: true,
+    centerMode: true,
+    focusOnSelect: true,
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    speed: 1000,
+  });
 
+  $('.slider__content a').fancybox({
+    caption: function (instance, item) {
+      return $(this).parent().find('.card-text').html();
+    }
+  });
 });
