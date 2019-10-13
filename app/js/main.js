2a47099e4b683f0bbca5aabd5d628d701017fb95
AndIReyf <andrey.prudius@hotmail.com>
2019-10-13 22:26:26 +0200

    Added team and slider Life

--- app/js/main.js
+++ app/js/main.js
@@ -7,8 +7,6 @@ $(function(){
     dots: true,
     arrows: false,
     speed: 1000,
-    autoplay: true,
-    autoplaySpeed: 5000,
   });
 
   $('.slider__content').slick({
@@ -28,4 +26,23 @@ $(function(){
       return $(this).parent().find('.card-text').html();
     }
   });
+
+   $('.slider__life-main').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     arrows: true,
+     fade: true,
+     asNavFor: '.slider__life-nav',
+     prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icon/left-arrow.svg"></button>',
+     nextArrow: '<button class="slick-arrow slick-next"><img src="images/icon/right-arrow.svg"></button>',
+   });
+   $('.slider__life-nav').slick({
+     slidesToShow: 5,
+     slidesToScroll: 1,
+     arrows: true,
+     asNavFor: '.slider__life-main',
+     focusOnSelect: true,
+     centerMode: true,
+     infinite: true,
+   });
 });
