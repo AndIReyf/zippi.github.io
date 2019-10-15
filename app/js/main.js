a3ea4acec20f7911ccacede9b6b60d4283605d26
AndIReyf <andrey.prudius@hotmail.com>
2019-10-15 21:56:25 +0200

    Added sing-up, blog, contact, footer, arrow-up

--- app/js/main.js
+++ app/js/main.js
@@ -45,4 +45,11 @@ $(function(){
      centerMode: true,
      infinite: true,
    });
+
+  $("#menu, #up").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
 });
